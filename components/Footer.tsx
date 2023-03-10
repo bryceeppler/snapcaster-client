b921b321670d3d1d4f23ba53a6fc07eae5b3fb96
Bryce Eppler <eppler97@gmail.com>
2023-03-10 15:52:20 -0800

    foot fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,6 +4,27 @@ type Props = {}
 
 export default function Footer({}: Props) {
   return (
-    <div>Footer</div>
+    <div
+      className="flex flex-row justify-center items-center
+      bg-zinc-900
+      text-zinc-100
+      text-sm
+      p-2
+      "
+    >
+      <div className="flex flex-row space-x-2">
+        <div className="text-zinc-100">
+          Created by {" "}
+          <a
+            href="https://www.bryceeppler.com"
+            target="_blank"
+            rel="noreferrer"
+            className="text-pink-500 hover:text-pink-700"
+          >
+            Bryce Eppler
+          </a>
+        </div>
+        </div>
+    </div>
   )
 }
