80d530b85727d395ce1d27c23b7ef3bbb5b751f9
Bryce Eppler <eppler97@gmail.com>
2023-03-10 17:16:00 -0800

    analytics bugfix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,6 @@ import { AppProps } from 'next/app';
 import { SessionContextProvider } from '@supabase/auth-helpers-react';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { Analytics } from '@vercel/analytics/react';
-
 import Layout from '@/components/Layout';
 import { MyUserContextProvider } from '@/utils/useUser';
 import type { Database } from 'types_db';
