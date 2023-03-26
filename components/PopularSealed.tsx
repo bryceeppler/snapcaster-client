4244eb600d3240c98eccc82a5081aeb23b5bfa6f
Bryce Eppler <eppler97@gmail.com>
2023-03-26 06:40:13 -0700

    sealed product carousel

--- components/PopularSealed.tsx
+++ components/PopularSealed.tsx
@@ -1,9 +1,109 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import LoadingDots from './ui/LoadingDots';
 
-type Props = {}
+type Props = {
+  popularSealed: SealedProductInfo[];
+};
 
-export default function PopularSealed({}: Props) {
+export type SealedProductInfo = {
+  name: string;
+  count: number;
+  product_name: string;
+  product_image: string;
+  product_price: number | null;
+};
+
+export default function PopularSealed({ popularSealed }: Props) {
+  // const [popularSealed, setPopularSealed] = useState<SealedProductInfo[]>([]);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  // useEffect(() => {
+  //   axios
+  //     .get(`${process.env.NEXT_PUBLIC_SNAPCASTER_API_URL}/utils/popular_cards/`)
+  //     .then((res) => {
+  //       let popularSealed = [...res.data.monthly, ...res.data.weekly];
+  //       // remove duplicates
+  //       popularSealed = popularSealed.filter(
+  //         (card, index, self) =>
+  //           index === self.findIndex((t) => t.name === card.name)
+  //       );
+  //       setPopularSealed(popularSealed);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //     });
+  // }, []);
+
+  const nextSealedProduct = () => {
+    setActiveIndex((prevIndex) => (prevIndex + 1) % popularSealed.length);
+  };
+
+  const prevSealedProduct = () => {
+    setActiveIndex(
+      (prevIndex) => (prevIndex - 1 + popularSealed.length) % popularSealed.length
+    );
+  };
+
+  const getSealedProductIndex = (offset: number) => {
+    const newIndex = activeIndex + offset;
+    return newIndex >= 0
+      ? newIndex % popularSealed.length
+      : popularSealed.length + newIndex;
+  };
+
+  const visibleSealed = [
+    popularSealed[getSealedProductIndex(-1)],
+    popularSealed[getSealedProductIndex(0)],
+    popularSealed[getSealedProductIndex(1)]
+  ];
+
   return (
-    <div>PopularSealed</div>
-  )
+    <div className="w-full max-w-3xl mx-auto mt-6">
+      <h2 className="text-2xl font-bold mb-4 text-center">Popular Sealed</h2>
+      {popularSealed.length > 0 ? (
+        <div className="flex justify-center items-center">
+          <button
+            className="bg-white bg-opacity-75 p-2 rounded-r"
+            onClick={prevSealedProduct}
+          >
+            &lt;
+          </button>
+          {visibleSealed.map((product, index) => (
+            <div
+              key={index}
+              className={`sm:w-1/3 mx-2 flex flex-col items-center ${
+                //  if small or below, hide all but index 0
+                index === 0 ? 'block' : 'hidden sm:block'
+              }`}
+            >
+              <div className="h-48 w-full flex items-center">
+                <img
+                  className="max-h-full max-w-full object-contain mx-auto"
+                  src={product.product_image}
+                  alt={product.product_name}
+                />
+              </div>
+              <p className="text-center mt-2 truncate w-36 text-sm">{product.product_name}</p>
+              <p className="text-center text-sm font-mono text-pink-300">
+                {product.product_price
+                  ? `$${product.product_price.toFixed(2)}`
+                  : 'Price not available'}
+              </p>
+            </div>
+          ))}
+          <button
+            className="bg-white bg-opacity-75 p-2 rounded-l"
+            onClick={nextSealedProduct}
+          >
+            &gt;
+          </button>
+        </div>
+      ) : (
+        <div className="flex items-center justify-center pt-5">
+          <LoadingDots />
+        </div>
+      )}
+    </div>
+  );
 }
--- pages/sealed.tsx
+++ pages/sealed.tsx
@@ -1,22 +1,32 @@
-import React from 'react'
-import Head from 'next/head'
-import Loadingspinner from '@/components/Loadingspinner'
-import SealedSearchBox from '@/components/SealedSearchBox'
-import { useUser } from '@/utils/useUser'
-import { useRouter } from 'next/router'
-import { useStore } from '@/store'
-import SealedCatalogRow from '@/components/SealedCatalogRow'
-import SealedSearchInfo from '@/components/SealedSearchInfo'
-import SealedSearchFilters from '@/components/SealedSearchFilters'
-type Props = {}
+import React from 'react';
+import Head from 'next/head';
+import Loadingspinner from '@/components/Loadingspinner';
+import SealedSearchBox from '@/components/SealedSearchBox';
+import { useUser } from '@/utils/useUser';
+import { useRouter } from 'next/router';
+import { useStore } from '@/store';
+import SealedCatalogRow from '@/components/SealedCatalogRow';
+import SealedSearchInfo from '@/components/SealedSearchInfo';
+import SealedSearchFilters from '@/components/SealedSearchFilters';
+import { SealedProductInfo } from '@/components/PopularSealed';
+import PopularSealed from '@/components/PopularSealed';
+import { GetServerSideProps } from 'next';
+import axios from 'axios';
+type Props = {
+  popularSealed: SealedProductInfo[];
+};
 
-export default function Sealed({}: Props) {
-  const { filteredSealedSearchResults : results, sealedSearchResultsLoading:loading, sealedSearchHasResults } = useStore()
-    const router = useRouter()
-    const showBanner = true;
-    const { user, isLoading, subscription } = useUser()
+export default function Sealed({ popularSealed }: Props) {
+  const {
+    filteredSealedSearchResults: results,
+    sealedSearchResultsLoading: loading,
+    sealedSearchHasResults
+  } = useStore();
+  const router = useRouter();
+  const showBanner = true;
+  const { user, isLoading, subscription } = useUser();
 
-    // if (subscription?.status === "active")
+  // if (subscription?.status === "active")
   return (
     <>
       <Head>
@@ -39,24 +49,28 @@ export default function Sealed({}: Props) {
               <Loadingspinner />
             </div>
           )}
-          
+          {
+            !sealedSearchHasResults && popularSealed.length > 0 && <PopularSealed popularSealed={popularSealed} />
+          }
+
           <div className="mt-2">
             {sealedSearchHasResults && (
-              <div>                <SealedSearchInfo />
-              <SealedSearchFilters /></div>
+              <div>
+                {' '}
+                <SealedSearchInfo />
+                <SealedSearchFilters />
+              </div>
             )}
             {results.length > 0 && (
               <div>
-
                 {results.map((result, index) => (
                   <div key={index}>
                     <SealedCatalogRow product={result} />
                   </div>
-                ))} 
+                ))}
               </div>
             )}
           </div>
-
         </div>
       </main>
     </>
@@ -76,14 +90,48 @@ export default function Sealed({}: Props) {
       </Head>
       <main className="flex flex-col justify-between items-center p-8 min-h-screen">
         <div className="flex-col justify-center flex-1 text-center max-w-xl w-full">
-            <div className="text-sm">Sealed search requires a premium membership.</div>
-            <div className="text-sm">Click <a
-            className="text-pink-500 hover:text-pink-700 cursor-pointer"
-              onClick={() => router.push("/pricing")}
-            >here</a> to view membership options.</div>
+          <div className="text-sm">
+            Sealed search requires a premium membership.
+          </div>
+          <div className="text-sm">
+            Click{' '}
+            <a
+              className="text-pink-500 hover:text-pink-700 cursor-pointer"
+              onClick={() => router.push('/pricing')}
+            >
+              here
+            </a>{' '}
+            to view membership options.
+          </div>
         </div>
       </main>
     </>
   );
 }
 
+export const getServerSideProps: GetServerSideProps = async () => {
+  try {
+    const res = await axios.get(
+      `${process.env.NEXT_PUBLIC_SNAPCASTER_API_URL}/utils/popular_sealed/`
+    );
+    let popularSealedProduct = [...res.data.monthly, ...res.data.weekly];
+    // remove duplicates
+    popularSealedProduct = popularSealedProduct.filter(
+      (prod, index, self) =>
+        index === self.findIndex((t) => t.name === prod.name)
+    );
+
+    return {
+      props: {
+        popularSealed: popularSealedProduct
+      }
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      props: {
+        popularSealed: []
+      }
+    };
+  }
+};
