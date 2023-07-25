988e4c35251d66594a13b521df293e143e5711be
Bryce Eppler <eppler97@gmail.com>
2023-07-25 05:23:23 -0700

    changing max 5 cards on multisearch

--- components/MultiSearchbox.tsx
+++ components/MultiSearchbox.tsx
@@ -1,119 +1,116 @@
-import React from 'react'
-import { useStore } from '@/store'
+import React, {useState} from 'react';
+import { useStore } from '@/store';
 
-type Props = {}
+type Props = {};
 
 const parseMultiSearchInput = (input: string) => {
-  const lines = input.split(/\n/)
-
-  let returnString = ''
+  const lines = input.split(/\n/);
+  let count = 0;
+  let returnString = '';
 
   for (let i = 0; i < lines.length; i++) {
-    const line = lines[i]
+    const line = lines[i];
     // if the line is empty, skip it
-    if (line === '') continue
+    if (line === '') continue;
 
-    // remove any numbers from the start of the line
-    const lineWithoutCount = line.replace(/^\d+/, '')
-    // remove any whitespace from the start of the line
-    const lineWithoutCountAndWhitespace = lineWithoutCount.replace(/^\s+/, '')
-    // remove any whitespace from the end of the line
-    const lineWithoutCountAndWhitespaceAndTrailingWhitespace = lineWithoutCountAndWhitespace.replace(/\s+$/, '')
-    // add the line to the return string
-    // if the length > 0, add to the return string
-    if (lineWithoutCountAndWhitespaceAndTrailingWhitespace.length > 0) {
-      returnString += lineWithoutCountAndWhitespaceAndTrailingWhitespace + '\n'
+    if (count < 5) {
+      // remove any numbers from the start of the line
+      const lineWithoutCount = line.replace(/^\d+/, '');
+      // remove any whitespace from the start of the line
+      const lineWithoutCountAndWhitespace = lineWithoutCount.replace(
+        /^\s+/,
+        ''
+      );
+      // remove any whitespace from the end of the line
+      const lineWithoutCountAndWhitespaceAndTrailingWhitespace =
+        lineWithoutCountAndWhitespace.replace(/\s+$/, '');
+      // add the line to the return string
+      // if the length > 0, add to the return string
+      if (lineWithoutCountAndWhitespaceAndTrailingWhitespace.length > 0) {
+        returnString +=
+          lineWithoutCountAndWhitespaceAndTrailingWhitespace + '\n';
+        count++;
+      }
     }
   }
 
-  return returnString
-  // const result = lines.map((line) => {
-  //   // if the line is empty, return null
-  //   if (line === '') return null
-  //   // if the line is a number, return null
-  //   if (!isNaN(parseInt(line))) return null
-  //   // remove any numbers from the start of the line
-  //   const lineWithoutCount = line.replace(/^\d+/, '')
-  //   // remove any whitespace from the start of the line
-  //   const lineWithoutCountAndWhitespace = lineWithoutCount.replace(/^\s+/, '')
-  //   // remove any whitespace from the end of the line
-  //   const lineWithoutCountAndWhitespaceAndTrailingWhitespace = lineWithoutCountAndWhitespace.replace(/\s+$/, '')
-  //   // return the line
-  //   return lineWithoutCountAndWhitespaceAndTrailingWhitespace
-  // })
-  // // filter out any null values
-  // result.filter((line) => line !== null)
-  // // join the lines with a newline
-  // console.log(result.join('\n')
-}
-
+  return returnString;
+};
 
 export default function MultiSearchbox({}: Props) {
-    const { multiSearchQuery, multiSearchInput, setMultiSearchInput, fetchMultiSearchResults } = useStore()
+  const {
+    multiSearchQuery,
+    multiSearchInput,
+    setMultiSearchInput,
+    fetchMultiSearchResults
+  } = useStore();
+  const [warning, setWarning] = useState(false);
+
   return (
-    <div className="flex flex-col w-full justify-center mb-4">
+    <div className="mb-4 flex w-full flex-col justify-center">
       <div className="mt-3 w-full">
-        {/* <label
-          htmlFor="multisearchFormControlTextarea1"
-          className="text-3xl font-extrabold form-label inline-block mb-2 "
-        >
-          Cards
-        </label> */}
-        <div className="text-sm text-gray-400 mb-2">
-          Enter card names, one per line (max 100 lines)
+        <div className="mb-2 text-sm text-gray-400">
+          Enter card names, one per line (max 5 lines)
         </div>
         <textarea
           className="
           form-control
+          m-0
           block
           w-full
-          px-3
-          py-1.5
+          rounded
+          border
+          border-solid
+          border-zinc-300 bg-zinc-900
+          bg-clip-padding px-3 py-1.5
           text-base
           font-normal
-          bg-zinc-900 bg-clip-padding
-          border border-solid border-zinc-300
-          rounded
           transition
           ease-in-out
-          m-0
-        focus:text-white focus:bg-black focus:border-pink-600 focus:outline-none
+        focus:border-pink-600 focus:bg-black focus:text-white focus:outline-none
         "
           id="multisearchFormControlTextarea1"
-          rows={10}
-          placeholder={`Enter card names, one per line (max 100 lines)
+          rows={6}
+          placeholder={`Enter card names, one per line (max 5 lines)
 1 Ajani's Chosen
 1 Arcane Signet
 Dockside Extortionist
 Counterspell`}
           value={multiSearchInput}
-          onChange={(e) => setMultiSearchInput(e.target.value)}
+          onChange={(e) => {
+            setMultiSearchInput(e.target.value);
+            setWarning(e.target.value.split(/\n/).filter(line => line.trim() !== '').length > 5);
+          }}
         ></textarea>
+                {warning && <div className="text-red-600 mt-2">Warning: Max 5 lines allowed!</div>}
+
       </div>
       <button
         className="
+            focus:shadow-outline
+            mx-auto
+            mt-4
+            rounded
             bg-pink-600
-            hover:bg-pink-700
-            text-white
-            font-bold
             py-2
             px-4
-            rounded
+            font-bold
+            text-white
+            hover:bg-pink-700
             focus:outline-none
-            focus:shadow-outline
-            mt-4
-            mx-auto
+            disabled:bg-gray-500
+            disabled:cursor-not-allowed
           "
         type="button"
+        disabled={multiSearchInput === '' || warning}
         // onClick={() => store.handleSubmit()}
         onClick={() => {
-          const result = parseMultiSearchInput(multiSearchInput)
-          fetchMultiSearchResults(result)
+          const result = parseMultiSearchInput(multiSearchInput);
+          fetchMultiSearchResults(result);
         }}
       >
         Search
       </button>
-    </div>  
-    )
-  }
-  
+    </div>
+  );
+}
