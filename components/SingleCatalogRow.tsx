92c26a99f0bf42420217569d9fe75423937d7613
Bryce Eppler <eppler97@gmail.com>
2023-11-16 05:52:13 -0800

    adding price data to buy button

--- components/SingleCatalogRow.tsx
+++ components/SingleCatalogRow.tsx
@@ -147,6 +147,7 @@ export default function SingleCatalogRow({ cardData }: Props) {
               <a
                 className="transition-all bg-zinc-800 hover:bg-zinc-900 text-white font-bold p-1 px-2 sm:py-1 sm:px-4 rounded focus:outline-none focus:shadow-outline mt-4 "
                 href={cardData.link}
+                data-price={cardData.price}
                 target="_blank"
                 id='buy-button'
                 
