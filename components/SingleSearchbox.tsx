9579495d9a7912423c5f46ef3aa1eb5951cfa906
Bryce Eppler <eppler97@gmail.com>
2024-02-12 16:08:04 -0800

    bugfix

--- components/SingleSearchbox.tsx
+++ components/SingleSearchbox.tsx
@@ -9,7 +9,7 @@ export default function SingleSearchbox({}: Props) {
     useStore();
   // const autocompleteEndpoint = "http://localhost/autocomplete/cards?query=";
   // load url from env vars
-  const autocompleteEndpoint = process.env.NEXT_PUBLIC_AUTOCOMPLETE_URL + "/autocomplete/cards?query=";
+  const autocompleteEndpoint = process.env.NEXT_PUBLIC_AUTOCOMPLETE_URL + "/cards?query=";
   const [autocompleteResults, setAutocompleteResults] = useState<string[]>([]);
   const [showAutocomplete, setShowAutocomplete] = useState(false);
   const [selectedAutocompleteIndex, setSelectedAutocompleteIndex] =
