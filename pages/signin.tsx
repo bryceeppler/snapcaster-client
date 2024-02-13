24e60da40b8d344bc60ca8d733fb16e5812b7ea7
Bryce Eppler <eppler97@gmail.com>
2024-02-13 14:38:59 -0800

    adding sign in page

---
+++ pages/signin.tsx
@@ -0,0 +1,84 @@
+import MainLayout from '@/components/MainLayout';
+import { type NextPage } from 'next';
+import Head from 'next/head';
+type Props = {};
+
+const Signin: NextPage<Props> = () => {
+  return (
+    <>
+      <SigninHead />
+      <MainLayout>
+        <div className="w-full max-w-md flex-1 flex-col justify-center text-center">
+          <section className="w-full py-6 md:py-12">
+            <div className="container grid max-[1fr_900px] md:px-6 items-start gap-6">
+              <div className="space-y-2">
+                <h2 className="text-3xl font-bold tracking-tighter">Signin</h2>
+                <p className="text-gray-500 dark:text-gray-400">
+                  Log in or create an account.
+                </p>
+              </div>
+              <div className="grid gap-4 md:gap-4">
+                <div className="relative">
+                  <input
+                    type="text"
+                    className={`block w-full rounded-md border border-zinc-300 px-4 py-2 placeholder-zinc-500 shadow-sm focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm text-white bg-zinc-800`}
+                    placeholder="Email"
+                  />
+                </div>
+                <div className="relative">
+                  <input
+                    type="password"
+                    className={`block w-full rounded-md border border-zinc-300 px-4 py-2 placeholder-zinc-500 shadow-sm focus:border-pink-500 focus:outline-none focus:ring-pink-500 sm:text-sm text-white bg-zinc-800`}
+                    placeholder="Password"
+                  />
+                </div>
+                <button
+                  onClick={() => {
+                    window.location.href = '/api/auth/signin';
+                  }}
+                  className="mt-2 p-2 bg-neutral-700 rounded-lg hover:bg-neutral-600"
+                >
+                  Signin
+                </button>
+                <button
+                  onClick={() => {
+                    window.location.href = '/api/auth/signin';
+                  }}
+                  className="mt-2 p-2 bg-neutral-700 rounded-lg hover:bg-neutral-600"
+                >
+                  Signup
+                </button>
+              </div>
+            </div>
+          </section>
+        </div>
+      </MainLayout>
+    </>
+  );
+};
+
+export default Signin;
+
+const SigninHead = () => {
+  return (
+    <Head>
+      <title>Signin</title>
+      <meta
+        name="description"
+        content="Search Magic the Gathering cards across Canada"
+      />
+      <meta
+        property="og:title"
+        content={`Snapcaster - Search Magic the Gathering cards across Canada`}
+      />
+      <meta
+        property="og:description"
+        content={`Find Magic the Gathering singles and sealed product using in Snapcaster. Search your favourite Canadian stores.`}
+      />
+      <meta property="og:url" content={`https://snapcaster.ca`} />
+      <meta property="og:type" content="website" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+};
