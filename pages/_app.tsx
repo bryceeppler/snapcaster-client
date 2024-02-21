a8c6b0d4a99d05781c98000508ed50b48b6d1851
Bryce Eppler <eppler97@gmail.com>
2024-02-20 16:01:31 -0800

    adding script tags for adsense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -63,6 +63,31 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         src={`${process.env.NEXT_PUBLIC_ADSENSE}`}
         crossOrigin="anonymous"
       />
+      {/* <script async src="https://fundingchoicesmessages.google.com/i/pub-6026504058618942?ers=1" nonce="W0DFASoiabMy4-_cYoMhEA"></script><script nonce="W0DFASoiabMy4-_cYoMhEA">(function() {function signalGooglefcPresent() {if (!window.frames['googlefcPresent']) {if (document.body) {const iframe = document.createElement('iframe'); iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;'; iframe.style.display = 'none'; iframe.name = 'googlefcPresent'; document.body.appendChild(iframe);} else {setTimeout(signalGooglefcPresent, 0);}}}signalGooglefcPresent();})();</script> */}
+      <Script id="googlefc" src="https://fundingchoicesmessages.google.com/i/pub-6026504058618942?ers=1" nonce="W0DFASoiabMy4-_cYoMhEA" />
+      <Script
+        id="googlefc"
+        nonce="W0DFASoiabMy4-_cYoMhEA"
+      >
+        {`
+          (function() {
+            function signalGooglefcPresent() {
+              if (!window.frames['googlefcPresent']) {
+                if (document.body) {
+                  const iframe = document.createElement('iframe');
+                  iframe.style = 'width: 0; height: 0; border: none; z-index: -1000; left: -1000px; top: -1000px;';
+                  iframe.style.display = 'none';
+                  iframe.name = 'googlefcPresent';
+                  document.body.appendChild(iframe);
+                } else {
+                  setTimeout(signalGooglefcPresent, 0);
+                }
+              }
+            }
+            signalGooglefcPresent();
+          })();
+        `}
+      </Script>
     </div>
   );
 }
