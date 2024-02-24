e763d53a76d95284670bc287668ec2db1a204aa4
Bryce Eppler <eppler97@gmail.com>
2024-02-24 08:36:53 -0800

    image modal for single search results

--- components/SingleCatalogRow.tsx
+++ components/SingleCatalogRow.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { SingleSearchResult } from '@/stores/store';
 type Props = {
   cardData: SingleSearchResult;
 };
+
 import { useStore } from '@/stores/store';
 import { trackOutboundLink } from '../utils/analytics';
 
 
 export default function SingleCatalogRow({ cardData }: Props) {
   const { websites } = useStore();
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  // ... other hooks and functions
+
+  const handleImageClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  // The modal component
+  const ImageModal = ({ src, alt }: { src: string, alt: string }) => {
+    if (!isModalOpen) return null;
+
+    return (
+      <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 p-4" onClick={closeModal}>
+      <div className="p-4 rounded-lg max-w-xl w-full shadow-lg">
+        <img 
+          src={src} 
+          alt={alt} 
+          className="max-w-full max-h-[70vh] h-auto rounded-lg mx-auto" 
+        />
+      </div>
+    </div>
+    );
+  };
+
 
   const findWebsiteNameByCode = (code: string): string => {
     const website = websites.find((website) => website.code === code);
@@ -26,12 +56,15 @@ export default function SingleCatalogRow({ cardData }: Props) {
       <>
         <div className="grid grid-cols-12 gap-4 my-2 p-2 sm:my-3 sm:p-4 rounded-md outline outline-1 outline-zinc-600 transition-all bg-zinc-900">
           <div className="col-span-3 flex">
-            <img
-              src={cardData.image}
-              alt="card"
-              className="w-16 md:w-24 rounded-md object-contain"
-            />
+          <img
+          src={cardData.image}
+          alt="card"
+          className="w-16 md:w-24 rounded-md object-contain cursor-pointer"
+          onClick={handleImageClick}
+        />
           </div>
+          {isModalOpen && <ImageModal src={cardData.image} alt="card" />}
+
           <div className="col-span-5 mt-2">
             <div className="flex flex-col text-left">
               <div className="text-sm">{cardData.set}</div>
