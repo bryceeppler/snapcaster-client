2430442ac30b9fb431185b09c2f2d80b76495725
Bryce Eppler <eppler97@gmail.com>
2024-05-09 17:13:03 -0700

    update home banner

--- components/homebanner.tsx
+++ components/homebanner.tsx
@@ -13,7 +13,7 @@ export default function Homebanner({}: Props) {
 
       <h1 className="mt-2 text-4xl font-extrabold">Snapcaster</h1>
       <p className="text-md text-gray-400">
-        Search for Magic the Gathering cards across Canada
+        Search for trading cards across Canada
       </p>
     </div>
   );
