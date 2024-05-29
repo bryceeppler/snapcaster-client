fe701a4da5d93005c46add331c807fd9eeebf943
Bryce Eppler <eppler97@gmail.com>
2024-05-28 18:45:45 -0700

    adding id elements to ads

--- components/main-page-layout.tsx
+++ components/main-page-layout.tsx
@@ -10,16 +10,31 @@ export default function MainLayout({
     <main className="container w-full max-w-5xl flex-1 flex-col items-center justify-center px-2 py-8">
       {adsEnabled && (
         <>
-          {/* top banner */}
-          <div className="flex h-40 w-full items-center justify-center rounded border border-zinc-600 bg-zinc-700">
+          {/* top banner : position 1 */}
+          <div
+            data-position-id="1"
+            data-ad-type="banner"
+            data-ad-id="top-banner"
+            className="flex h-40 w-full items-center justify-center rounded border border-zinc-600 bg-zinc-700"
+          >
             Tier 1 Ad
           </div>
-          {/* left ad */}
-          <div className="fixed left-10 top-1/4 hidden h-1/2 w-40 items-center justify-center rounded border border-zinc-600 bg-zinc-700 xl:flex">
+          {/* left ad : position 2 */}
+          <div
+            data-position-id="2"
+            data-ad-type="banner"
+            data-ad-id="left-banner"
+            className="fixed left-10 top-1/4 hidden h-1/2 w-40 items-center justify-center rounded border border-zinc-600 bg-zinc-700 xl:flex"
+          >
             Tier 1 Ad
           </div>
-          {/* right ad */}
-          <div className="fixed right-10 top-1/4 hidden h-1/2 w-40 items-center justify-center rounded border border-zinc-600 bg-zinc-700 xl:flex xl:flex-col">
+          {/* right ad : position 3 */}
+          <div
+            data-position-id="3"
+            data-ad-type="banner"
+            data-ad-id="right-banner"
+            className="fixed right-10 top-1/4 hidden h-1/2 w-40 items-center justify-center rounded border border-zinc-600 bg-zinc-700 xl:flex xl:flex-col"
+          >
             <span className="flex flex-grow items-center justify-center">
               Tier 2 Ad
             </span>
