146beda316b0fa44965b9a18f6208dd9c7da0298
Bryce Eppler <eppler97@gmail.com>
2024-05-30 11:53:21 -0700

    adjusting analytics logging

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -60,7 +60,7 @@ export const handleQuerySingleCard = (
 };
 
 export const trackAdVisible = (adId: string) => {
-  console.log('Pushing ad visible event');
+  console.log('Pushing ad visible event: ', adId);
   window.dataLayer.push({
     event: 'ad_visible',
     ad_id: adId
