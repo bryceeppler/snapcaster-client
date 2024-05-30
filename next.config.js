0f06ede1924536425335866ac0db37b4ab408c4b
Bryce Eppler <eppler97@gmail.com>
2024-05-30 09:33:54 -0700

    cors policY

--- next.config.js
+++ next.config.js
@@ -14,6 +14,23 @@ const nextConfig = {
         protocol: 'http'
       }
     ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp'
+          },
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin'
+          }
+        ]
+      }
+    ];
   }
 };
 
