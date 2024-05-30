55dd54af012885676ec1f38cb590a5fafed51083
Bryce Eppler <eppler97@gmail.com>
2024-05-30 09:38:47 -0700

    updating cors

--- next.config.js
+++ next.config.js
@@ -14,6 +14,27 @@ const nextConfig = {
         protocol: 'http'
       }
     ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // TODO change this
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,DELETE,PATCH,POST,PUT'
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+          }
+        ]
+      }
+    ];
   }
 };
 
