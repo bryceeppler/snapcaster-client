66a2060d8706bc417658b04451a93a626f815a38
Bryce Eppler <eppler97@gmail.com>
2024-05-30 09:24:52 -0700

    ad test page

--- pages/adtester.tsx
+++ pages/adtester.tsx
@@ -1,43 +1,25 @@
 import { useState } from 'react';
-import SingleCatalogRow from '@/components/single-search/single-list-item';
 import { SingleSearchResult } from '@/stores/store';
-import BannerAd from '@/components/ad';
-import { useEffect } from 'react';
+import Link from 'next/link';
+import useGlobalStore from '@/stores/globalStore';
+import { handleAdClick } from '@/utils/analytics';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious
+} from '@/components/ui/carousel';
 
-type CatalogRowProps = {
-  cardData: SingleSearchResult;
-  promoted?: boolean;
-  tcg: string;
-};
-const ads = [
-  {
-    positionId: 'top-banner',
-    adType: 'horizontal-banner',
-    adId: '1',
-    href: 'https://obsidiangames.ca/',
-    images: {
-      desktop:
-        'https://s3.ca-central-1.amazonaws.com/cdn.snapcaster.ca/obsidian_1008x160.png',
-      mobile:
-        'https://s3.ca-central-1.amazonaws.com/cdn.snapcaster.ca/obsidian_1008x160.png'
-    }
-  },
-  {
-    positionId: 'left-banner',
-    adType: 'vertical-banner',
-    adId: '2',
-    href: 'https://company2.com/',
-    images: {
-      desktop:
-        'https://s3.ca-central-1.amazonaws.com/cdn.snapcaster.ca/obsidian_1008x160.png',
-      mobile:
-        'https://s3.ca-central-1.amazonaws.com/cdn.snapcaster.ca/obsidian_382x160.png'
-    }
-  }
-];
 export default function Component() {
   const [currentAdIndex, setCurrentAdIndex] = useState(0);
-
+  const { ads } = useGlobalStore();
+  if (!ads.position || Object.keys(ads.position).length === 0) {
+    return null; // or a loading spinner
+  }
+  const topBannerAd = ads.position['1'].ads[0];
+  const leftBannerAd = ads.position['2']?.ads[0]; // Assuming there's only one ad in position 2
+  const carouselAds = ads.position['3']?.ads || [];
   const cardData = {
     name: 'test',
     price: 10,
@@ -47,38 +29,71 @@ export default function Component() {
     website: 'testwebsite'
   } as SingleSearchResult;
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentAdIndex((prevIndex) => (prevIndex + 1) % ads.length);
-    }, 10000); // Rotate every 10 seconds
-  }, []);
-
   return (
     <section className="mx-auto flex h-screen w-full max-w-5xl flex-col items-center space-y-8 text-center">
       <p>This is for testing analytics on prod</p>
       <h1 className="text-3xl font-bold">Ad Tester</h1>
 
-      <BannerAd
-        positionId="top-banner"
-        adType="horizontal-banner"
-        adId={ads[currentAdIndex].adId}
-        href={ads[currentAdIndex].href}
+      {/* Header : position 1 */}
+      <Link
+        href={topBannerAd.url}
+        target="_blank"
+        data-position-id="1"
+        data-ad-id={topBannerAd.id.toString()}
+        className="ad flex items-center justify-center rounded border border-zinc-600 bg-black"
       >
         <img
           className="hidden h-fit w-full md:flex"
-          src={ads[currentAdIndex].images.desktop}
+          src={topBannerAd.desktop_image}
           alt="ad"
         />
         <img
           className="flex h-full w-full object-cover md:hidden"
-          src={ads[currentAdIndex].images.mobile}
+          src={topBannerAd.mobile_image}
           alt="ad"
         />
-      </BannerAd>
+      </Link>
 
-      {/* Buy button tester */}
-      <h1 className="text-3xl font-bold">Buy Button Tester</h1>
-      <SingleCatalogRow cardData={cardData} tcg="mtg" />
+      {/* Left ad : position 2 */}
+      {leftBannerAd && (
+        <Link
+          href={leftBannerAd.url}
+          target="_blank"
+          data-position-id="2"
+          data-ad-id={leftBannerAd.id.toString()}
+          className="ad fixed left-10 top-1/4 hidden w-40 items-center justify-center rounded border border-zinc-600 bg-zinc-700 xl:flex xl:flex-col"
+        >
+          <img src={leftBannerAd.mobile_image} alt="ad" className="h-full" />
+        </Link>
+      )}
+
+      {/* right ad : position 3 */}
+      {carouselAds.length > 0 && (
+        <Carousel
+          data-position-id="3"
+          data-ad-type="carousel"
+          data-ad-id={carouselAds[currentAdIndex].id.toString()}
+          className={`ad fixed right-10 top-1/4 hidden max-h-[480px] w-40 items-center justify-center rounded border border-zinc-600 bg-zinc-700 xl:flex xl:flex-col`}
+          onClick={() =>
+            handleAdClick(
+              carouselAds[currentAdIndex].position,
+              'carousel',
+              carouselAds[currentAdIndex].id.toString(),
+              carouselAds[currentAdIndex].url
+            )
+          }
+        >
+          <CarouselContent>
+            {carouselAds.map((ad, index) => (
+              <CarouselItem key={index}>
+                <img src={ad.mobile_image} alt="ad" />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious className="" />
+          <CarouselNext className="" />
+        </Carousel>
+      )}
     </section>
   );
 }
