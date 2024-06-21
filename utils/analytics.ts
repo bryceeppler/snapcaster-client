b2e38c7044a7c0d46c8cef62f5bba73ee44e0121
Bryce Eppler <eppler97@gmail.com>
2024-06-21 12:42:51 -0700

    remove console logs

--- utils/analytics.ts
+++ utils/analytics.ts
@@ -60,7 +60,6 @@ export const handleQuerySingleCard = (
 };
 
 export const trackAdVisible = (adId: string) => {
-  console.log('ad visible');
   window.dataLayer = window.dataLayer || [];
   window.dataLayer.push({
     event: 'ad_visible',
