99215c6910dc5dcab443bba4b609c9373d19b48f
Bryce Eppler <eppler97@gmail.com>
2024-08-06 16:20:10 -0700

    handleBuyClicks analytics default to false promoted cards in advanced search

--- components/advanced-search/advanced-catalog-row.tsx
+++ components/advanced-search/advanced-catalog-row.tsx
@@ -156,6 +156,7 @@ export default function SingleCatalogRow({ cardData, tcg }: Props) {
                     cardData.link,
                     cardData.price,
                     cardData.name,
+                    false,
                     tcg
                   );
                 }}
