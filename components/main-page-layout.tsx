af833f460978decb18e34f62ededa07c5392ebf4
Bryce Eppler <eppler97@gmail.com>
2024-08-06 15:36:24 -0700

    adjust ad carousel timing

--- components/main-page-layout.tsx
+++ components/main-page-layout.tsx
@@ -30,7 +30,7 @@ export default function MainLayout({
   const { hasActiveSubscription } = useAuthStore();
   const plugin = React.useRef(
     Autoplay({
-      delay: 9000,
+      delay: 20000,
       stopOnInteraction: true
     }) as any
   );
