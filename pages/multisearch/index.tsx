6e3ccd989af7d76727a88af0f3be308984cc8aae
Bryce Eppler <eppler97@gmail.com>
2024-09-01 15:06:49 -0700

    all stores in recommended stores for multisearch

--- pages/multisearch/index.tsx
+++ pages/multisearch/index.tsx
@@ -298,7 +298,7 @@ const ResultSelector = () => {
     useMultiSearchStore();
   const { getWebsiteName } = useGlobalStore();
   const totalRequested = results.length;
-  const reccomendedWebsites = ['obsidian', 'levelup', 'chimera', 'exorgames'];
+  const reccomendedWebsites = ['obsidian', 'levelup', 'chimera', 'exorgames', 'mythicstore','houseofcards'];
   const [selectedTopStore, setSelectedTopStore] = useState('');
   const getTopWebsites = (results: MultiSearchProduct[]) => {
     const websiteProductSet: { [website: string]: Set<string> } = {};
