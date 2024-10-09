1cec8370131e464b558ea86b787a7150aa627649
Bryce Eppler <eppler97@gmail.com>
2024-10-09 04:28:31 -0700

    Multisearch not found list only display when len > 0

--- components/multi-search/results-container.tsx
+++ components/multi-search/results-container.tsx
@@ -8,21 +8,21 @@ export const ResultsContainer = ({ results }: { results: Product[][] }) => {
   const { notFound, resultsList } = useMultiSearchStore();
   return (
     <div className="flex w-full flex-col gap-4 rounded-lg border border-border bg-popover p-4">
-      <div className="flex w-full flex-row rounded-lg border border-destructive bg-destructive/20 p-4">
-        <AlertTriangleIcon className="mr-4 h-6 w-6" />
-        <div className="flex flex-col gap-2 text-left">
-          <h2 className="font-semibold">
-            Could not find the following cards...
-          </h2>
-          <ul
-            className="flex flex-col gap-2 list-disc list-inside"
-          >
-            {notFound.map((name, index) => {
-              return <li key={index}>{name}</li>;
-            })}
-          </ul>
+      {notFound.length > 0 && (
+        <div className="flex w-full flex-row rounded-lg border border-destructive bg-destructive/20 p-4">
+          <AlertTriangleIcon className="mr-4 h-6 w-6" />
+          <div className="flex flex-col gap-2 text-left">
+            <h2 className="font-semibold">
+              Could not find the following cards...
+            </h2>
+            <ul className="flex list-inside list-disc flex-col gap-2">
+              {notFound.map((name, index) => {
+                return <li key={index}>{name}</li>;
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
+      )}
       {resultsList.map((result, index) => {
         if (!results[index]) {
           return null;
