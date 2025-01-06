6beb02b0f7101891963ad4124e5fa03f0af8d593
Bryce Eppler <eppler97@gmail.com>
2025-01-06 13:48:06 +1000

    Only render icons for websites where an icon is present

--- components/single-search/single-catalog-item.tsx
+++ components/single-search/single-catalog-item.tsx
@@ -101,9 +101,9 @@ const SingleCatalogItem = ({ product }: Props) => {
               const matchingWebsite = websites.find(
                 (website) => product.vendor === website.slug
               );
-              return matchingWebsite ? (
+              return matchingWebsite?.meta?.branding?.icons ? (
                 <img
-                    src={theme === 'dark' ? matchingWebsite.meta?.branding?.icons?.dark : matchingWebsite.meta?.branding?.icons?.light}
+                  src={theme === 'dark' ? matchingWebsite.meta.branding.icons.dark : matchingWebsite.meta.branding.icons.light}
                   alt="Website"
                   className="h-4 w-4"
                 />
