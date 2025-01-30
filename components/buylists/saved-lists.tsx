66a8900ec1d3ac8a5cc0cd969cbca6b61728ab19
Bryce Eppler <eppler97@gmail.com>
2025-01-30 18:08:48 +1000

    Step 1 UI adjustments

--- components/buylists/saved-lists.tsx
+++ components/buylists/saved-lists.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
-import useBuyListStore from '@/stores/buyListStore';
+import useBuyListStore, { IBuylistCart } from '@/stores/buyListStore';
 import CardImage from '../ui/card-image';
 import { ScrollArea } from '../ui/scroll-area';
-import { MinusIcon, PlusIcon, SquarePen } from 'lucide-react';
+import { MinusIcon, PlusIcon, SquarePen, Trash } from 'lucide-react';
 import { useDebounceCallback } from 'usehooks-ts';
 import {
   Select,
@@ -20,14 +20,35 @@ import {
 } from '../ui/dialog';
 import { Button } from '../ui/button';
 import { Input } from '../ui/input';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger
+} from '@/components/ui/accordion';
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle
+} from '@/components/ui/alert-dialog';
 
-export default function SavedLists() {
+export default function SavedLists({
+  setCurrentStep
+}: {
+  setCurrentStep: (step: number) => void;
+}) {
   const {
     updateCartItemPending,
     updateCartItemAPI,
     currentCart,
     currentCartData,
     setCurrentCart,
+    getCartData,
     carts,
     deleteCart,
     renameCart,
@@ -42,137 +63,229 @@ export default function SavedLists() {
     updateCartItemPending(card, quantity);
     debouncedApiCall(card, quantity);
   };
+  const [cartToDelete, setCartToDelete] = useState<any>(null);
 
   return (
     <>
       <div className="min-h-svh-80  mb-8 ">
-        <div className="mb-1 flex flex-col gap-1 rounded-lg border bg-popover px-2 py-1">
-          {/* <div className=" text-center text-lg font-medium">
-            Your Saved Carts
-          </div> */}
-          <div>
-            <Select
-              value={currentCart?.id?.toString()}
-              onValueChange={(value) => {
-                const selectedCart = carts.find(
-                  (cart: any) => cart.id.toString() === value
-                );
-                setCurrentCart(selectedCart);
-              }}
-            >
-              <SelectTrigger className="border-grey-500 w-full  bg-popover focus:ring-0 focus:ring-offset-0">
-                <SelectValue placeholder="Please Select a List" />
-              </SelectTrigger>
-              <SelectContent>
-                {carts &&
-                  carts.map((cart: any) => (
+        {carts && carts.length === 0 && (
+          <div className="my-8 flex flex-col items-center justify-center">
+            <h3 className="text-2xl font-bold">You have no saved buylists</h3>
+            <p className="mb-4 text-sm text-muted-foreground">
+              Here you can create and manage your saved buylists. Create a
+              buylist to get started!
+            </p>
+            <Button onClick={() => setCreateDialogOpen(true)}>
+              Create new list
+            </Button>
+          </div>
+        )}
+        {carts && carts.length > 0 && (
+          <div className="mb-1 flex flex-col gap-4 px-2 py-1">
+            <div className="flex flex-row items-center justify-end">
+              <Button
+                className="w-fit"
+                onClick={() => setCreateDialogOpen(true)}
+              >
+                <PlusIcon className="mr-2 h-4 w-4" /> New List
+              </Button>
+            </div>
+            <div className="flex flex-col gap-4">
+              {carts.map((cart: IBuylistCart) => (
+                <div
+                  key={cart.id}
+                  className="flex flex-col rounded border bg-popover"
+                >
+                  <div
+                    className="flex cursor-pointer flex-row items-center justify-between border-b p-3 transition-all duration-300 hover:bg-background/30"
+                    onClick={() => {
+                      setCurrentCart(cart);
+                      setCurrentStep(1);
+                    }}
+                  >
+                    <span className="font-medium">{cart.name}</span>
+                    <div className="flex flex-row gap-2">
+                      <Button
+                        size="icon"
+                        className="h-6 w-6 p-0"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          setCurrentCart(cart);
+                          setActiveDialogId(cart.id);
+                        }}
+                      >
+                        <SquarePen className="h-4 w-4" />
+                        <span className="sr-only">Edit</span>
+                      </Button>
+
+                      <Button
+                        variant="ghost"
+                        size="icon"
+                        className="h-6 w-6 p-0"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          setCartToDelete(cart);
+                        }}
+                      >
+                        <Trash className="h-4 w-4" />
+                        <span className="sr-only">Delete</span>
+                      </Button>
+                    </div>
+                  </div>
+                  <div className="p-3">
+                    <Accordion type="single" collapsible className="w-full">
+                      <AccordionItem value="items" className="border-none">
+                        <AccordionTrigger
+                          className={`group flex items-center gap-2 py-0 ${
+                            cart.items?.length ? 'hover:no-underline cursor-pointer' : 'cursor-default hover:no-underline [&>svg]:hidden'
+                          }`}
+                          onClick={(e) => cart.items?.length && e.stopPropagation()}
+                          disabled={!cart.items?.length}
+                        >
+                          <span className="text-sm text-popover-foreground transition-colors group-hover:text-primary">
+                            {cart.items?.length || 0} items
+                          </span>
+                          {cart.items?.length > 0 && (
+                            <span className="text-xs text-popover-foreground transition-colors group-hover:text-primary/50">
+                              (click to view)
+                            </span>
+                          )}
+                        </AccordionTrigger>
+                        <AccordionContent className="pb-0">
+                          <div className="mt-2 space-y-2 rounded-lg bg-background p-2 text-sm text-muted-foreground">
+                            {cart.items
+                              ?.slice(0, 20)
+                              .map((item, index: number) => (
+                                <div
+                                  key={index}
+                                  className="flex flex-col pl-2 text-foreground"
+                                >
+                                  <span>
+                                    {item.quantity} x {item.card_name} (
+                                    {item.condition_name}){' '}
+                                    {item.foil ? '(Foil)' : ''}
+                                  </span>
+                                  <span className="text-xs text-muted-foreground">
+                                    {item.set_name}
+                                  </span>
+                                </div>
+                              ))}
+                            {cart.items && cart.items.length > 20 && (
+                              <div className="pl-2 italic text-muted-foreground/50">
+                                ...and {cart.items.length - 20} more
+                              </div>
+                            )}
+                          </div>
+                        </AccordionContent>
+                      </AccordionItem>
+                    </Accordion>
+                  </div>
+                </div>
+              ))}
+              {/* <Select
+                value={currentCart?.id?.toString()}
+                onValueChange={(value) => {
+                  const selectedCart = carts.find(
+                    (cart: any) => cart.id.toString() === value
+                  );
+                  setCurrentCart(selectedCart);
+                }}
+              >
+                <SelectTrigger className="border-grey-500 w-full  bg-popover focus:ring-0 focus:ring-offset-0">
+                  <SelectValue placeholder="Select a list" />
+                </SelectTrigger>
+                <SelectContent>
+                  {carts.map((cart: any) => (
                     <SelectItem key={cart.id} value={cart.id.toString()}>
                       {cart.name}
                     </SelectItem>
                   ))}
-              </SelectContent>
-            </Select>
+                </SelectContent>
+              </Select> */}
+            </div>
+            <div className="flex gap-2"></div>
           </div>
-          <div className="flex gap-2">
-            <Dialog open={createDialogOpen} onOpenChange={setCreateDialogOpen}>
-              <DialogTrigger asChild className="focus:ring-0 ">
-                <Button className="   w-full gap-2 border  " variant={'ghost'}>
-                  <PlusIcon className="size-5" />
-                  Create New Cart
-                </Button>
-              </DialogTrigger>
-
-              <DialogContent className=" max-w-sm md:max-w-lg">
-                <DialogHeader>
-                  <DialogTitle>Create New Cart</DialogTitle>
-                </DialogHeader>
-                <div className="flex flex-col gap-4">
-                  <Input
-                    placeholder="Cart name"
-                    value={newCartName}
-                    onChange={(e) => setNewCartName(e.target.value)}
-                  />
-                  <Button
-                    onClick={async () => {
-                      if (newCartName) {
-                        await createCart(newCartName);
-                        setNewCartName('');
-                        setCreateDialogOpen(false);
-                      }
-                    }}
-                  >
-                    Create
-                  </Button>
-                </div>
-              </DialogContent>
-            </Dialog>
-            <Dialog
-              open={currentCart ? activeDialogId === currentCart.id : false}
-              onOpenChange={(open) => {
-                if (open && currentCart) {
-                  setActiveDialogId(currentCart.id);
-                  setNewCartName(currentCart.name);
-                } else {
-                  setActiveDialogId(null);
-                  setNewCartName('');
-                }
-              }}
-            >
-              <DialogTrigger asChild className="focus:ring-0 ">
-                <Button
-                  className="   w-full gap-2 border  "
-                  variant={'ghost'}
-                  disabled={!currentCart}
-                >
-                  <SquarePen className="size-5" />
-                  Edit Cart
-                </Button>
-              </DialogTrigger>
-              <DialogContent className="max-w-sm md:max-w-lg">
-                <DialogHeader>
-                  <DialogTitle className="text-center">
-                    {currentCart?.name}
-                  </DialogTitle>
-                </DialogHeader>
-                <Input
-                  placeholder="New Cart Name"
-                  value={newCartName}
-                  onChange={(e) => setNewCartName(e.target.value)}
-                />
-                <div className="mt-2 flex justify-between gap-2">
-                  <Button
-                    disabled={!newCartName || newCartName === currentCart?.name}
-                    onClick={async () => {
-                      if (newCartName && currentCart) {
-                        const success = await renameCart({
-                          id: currentCart.id,
-                          name: newCartName
-                        });
-                        if (success) {
-                          setNewCartName('');
-                          setActiveDialogId(null);
-                        }
-                      }
-                    }}
-                  >
-                    Rename Cart
-                  </Button>
-                  <Button
-                    variant="destructive"
-                    onClick={async () => {
-                      await deleteCart(currentCart.id);
+        )}
+        <Dialog open={createDialogOpen} onOpenChange={setCreateDialogOpen}>
+          <DialogContent className=" max-w-sm md:max-w-lg">
+            <DialogHeader>
+              <DialogTitle>Create New Cart</DialogTitle>
+            </DialogHeader>
+            <div className="flex flex-col gap-4">
+              <Input
+                placeholder="Cart name"
+                value={newCartName}
+                onChange={(e) => setNewCartName(e.target.value)}
+              />
+              <Button
+                onClick={async () => {
+                  if (newCartName) {
+                    await createCart(newCartName);
+                    setNewCartName('');
+                    setCreateDialogOpen(false);
+                  }
+                }}
+              >
+                Create
+              </Button>
+            </div>
+          </DialogContent>
+        </Dialog>
+        <Dialog
+          open={currentCart ? activeDialogId === currentCart.id : false}
+          onOpenChange={(open) => {
+            if (open && currentCart) {
+              setActiveDialogId(currentCart.id);
+              setNewCartName(currentCart.name);
+            } else {
+              setActiveDialogId(null);
+              setNewCartName('');
+            }
+          }}
+        >
+          <DialogContent className="max-w-sm md:max-w-lg">
+            <DialogHeader>
+              <DialogTitle className="text-center">
+                {currentCart?.name}
+              </DialogTitle>
+            </DialogHeader>
+            <Input
+              placeholder="New Cart Name"
+              value={newCartName}
+              onChange={(e) => setNewCartName(e.target.value)}
+            />
+            <div className="mt-2 flex justify-between gap-2">
+              <Button
+                disabled={!newCartName || newCartName === currentCart?.name}
+                onClick={async () => {
+                  if (newCartName && currentCart) {
+                    const success = await renameCart({
+                      id: currentCart.id,
+                      name: newCartName
+                    });
+                    if (success) {
+                      setNewCartName('');
                       setActiveDialogId(null);
-                    }}
-                  >
-                    Delete Cart
-                  </Button>
-                </div>
-              </DialogContent>
-            </Dialog>
-          </div>
-        </div>
+                    }
+                  }
+                }}
+              >
+                Rename Cart
+              </Button>
+              <Button
+                variant="destructive"
+                onClick={async () => {
+                  await deleteCart(currentCart.id);
+                  setActiveDialogId(null);
+                }}
+              >
+                Delete Cart
+              </Button>
+            </div>
+          </DialogContent>
+        </Dialog>
 
-        <div className="rounded-lg border bg-popover px-3 py-1">
+        {/* <div className="rounded-lg border bg-popover px-3 py-1">
           <h1 className="text-center text-2xl font-bold"></h1>
 
           <ScrollArea className="h-[70svh]">
@@ -227,8 +340,35 @@ export default function SavedLists() {
                 ))}
             </div>
           </ScrollArea>
-        </div>
+        </div> */}
       </div>
+      <AlertDialog
+        open={!!cartToDelete}
+        onOpenChange={(open) => !open && setCartToDelete(null)}
+      >
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This will permanently delete the list "{cartToDelete?.name}" and
+              all its contents. This action cannot be undone.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancel</AlertDialogCancel>
+            <AlertDialogAction
+              onClick={async () => {
+                if (cartToDelete) {
+                  await deleteCart(cartToDelete.id);
+                  setCartToDelete(null);
+                }
+              }}
+            >
+              Delete List
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
     </>
   );
 }
