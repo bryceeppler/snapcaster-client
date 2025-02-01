4cd0a3a4fb3f457d230809218b940e493a9295e7
henry-minh <95643389+henry-minh@users.noreply.github.com>
2025-02-01 07:19:53 -0500

    attempting to fix the touchscreen click on the SelectItem component

--- components/ui/select.tsx
+++ components/ui/select.tsx
@@ -163,9 +163,13 @@ const SelectItem = React.forwardRef<
   <SelectPrimitive.Item
     ref={ref}
     className={cn(
-      'relative flex w-full cursor-pointer touch-auto select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50',
+      'relative flex w-full select-none items-center rounded-sm py-1.5 pl-8 pr-2 text-sm outline-none focus:bg-accent focus:text-accent-foreground data-[disabled=true]:pointer-events-none data-[disabled=true]:opacity-50',
       className
     )}
+    onTouchStart={(e) => {
+      e.preventDefault();
+      e.currentTarget.click();
+    }}
     {...props}
   >
     <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
