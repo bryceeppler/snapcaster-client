a49aac6f0206eaf9e538ee08520ae246d3887c89
henry-minh <95643389+henry-minh@users.noreply.github.com>
2025-02-18 01:53:06 -0500

    removed blog component and various attribute typos

--- pages/welcome.tsx
+++ pages/welcome.tsx
@@ -284,9 +284,9 @@ const ProblemSection = () => {
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                       className="lucide lucide-zap h-6 w-6 text-primary"
                     >
                       <path d="M4 14a1 1 0 0 1-.78-1.63l9.9-10.2a.5.5 0 0 1 .86.46l-1.92 6.02A1 1 0 0 0 13 10h7a1 1 0 0 1 .78 1.63l-9.9 10.2a.5.5 0 0 1-.86-.46l1.92-6.02A1 1 0 0 0 11 14z"></path>
